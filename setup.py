d73438c258d8b2c79a049343a5ad63c2c8f22180
Mateusz <m.z.matelski@gmail.com>
2020-11-05 21:23:02 +0100

    updated version

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import setuptools
 setuptools.setup(
     name='sqldiff',
     packages=setuptools.find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     description='Compare sql objects structure (tables, views or queries) on different databases',
     author='Mateusz Matelski',
     author_email='m.z.matelski@gmail.com',
     url='https://github.com/m-matelski/sqldiff',
-    download_url = 'https://github.com/m-matelski/sqldiff/archive/v0.0.1.tar.gz',
+    download_url = 'https://github.com/m-matelski/sqldiff/archive/v0.0.2.tar.gz',
     keywords = ['sql', 'diff', 'postgres', 'teradata'],
     install_requires=[
         'sqlparse==0.4.1', 'packaging==20.4'
